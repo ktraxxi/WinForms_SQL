e9830f8c6b83b8680d6fd4d1e78e022a7859725b
ktraxxi <rrkh96@mail.ru>
2022-01-08 21:55:27 +0300

    2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,17 +43,27 @@ namespace WindowsFormsSqlLearning
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.textBox7 = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
@@ -186,6 +196,68 @@ namespace WindowsFormsSqlLearning
             this.textBox7.Size = new System.Drawing.Size(780, 26);
             this.textBox7.TabIndex = 2;
             // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.tableLayoutPanel2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(792, 424);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "SQLDataReader";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.button3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.listView1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(792, 424);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(786, 375);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 35);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "SELECT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ProductName";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "QuantityPerUnit";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "UnitPrice";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -202,6 +274,8 @@ namespace WindowsFormsSqlLearning
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -222,6 +296,13 @@ namespace WindowsFormsSqlLearning
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -20,49 +20,67 @@ namespace WindowsFormsSqlLearning
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
             sqlConnection.Open();
-
             NorthWindConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthWindDB"].ConnectionString);
             NorthWindConnection.Open();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            //SqlCommand command = new SqlCommand
-            //    ($"INSERT INTO Students (name, surname, birthday) VALUES (N'{textBox1.Text}', N'{textBox2.Text}', '{textBox3.Text}')", 
-            //    sqlConnection);
-
             SqlCommand command = new SqlCommand
                 ($"INSERT INTO Students (name, surname, birthday, birthplace, phone, email) VALUES (@name, @surname, @birthday, @birthplace, @phone, @email)",
                 sqlConnection);
 
             DateTime date = DateTime.Parse(textBox3.Text);
 
-            command.Parameters.AddWithValue("name", textBox1.Text);
-            command.Parameters.AddWithValue("surname", textBox2.Text);
-            command.Parameters.AddWithValue("birthday", $"{date.Month}/{date.Day}/{date.Year}");
-            command.Parameters.AddWithValue("birthplace", textBox4.Text);
-            command.Parameters.AddWithValue("phone", textBox5.Text);
-            command.Parameters.AddWithValue("email", textBox6.Text);
+            command.Parameters.AddWithValue("name", textBox1.Text); command.Parameters.AddWithValue("surname", textBox2.Text);
+            command.Parameters.AddWithValue("birthday", $"{date.Month}/{date.Day}/{date.Year}"); command.Parameters.AddWithValue("birthplace", textBox4.Text);
+            command.Parameters.AddWithValue("phone", textBox5.Text); command.Parameters.AddWithValue("email", textBox6.Text);
 
             MessageBox.Show(command.ExecuteNonQuery().ToString());
-
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            SqlDataAdapter dataAdapter = new SqlDataAdapter(
-                textBox7.Text,
-                NorthWindConnection);
-
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(textBox7.Text, NorthWindConnection);
             DataSet dataSet = new DataSet();
             dataAdapter.Fill(dataSet);
-
             dataGridView1.DataSource = dataSet.Tables[0];
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            SqlDataReader dataReader = null;
+
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT ProductName, QuantityPerUnit, UnitPrice FROM Products", NorthWindConnection);
+                dataReader = sqlCommand.ExecuteReader();
+
+                ListViewItem item = null;
+                while (dataReader.Read())
+                {
+                    item = new ListViewItem(new string[] 
+                    { 
+                        Convert.ToString(dataReader["ProductName"]), 
+                        Convert.ToString(dataReader["QuantityPerUnit"]), 
+                        Convert.ToString(dataReader["UnitPrice"]) 
+                    });
+                    listView1.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (dataReader != null && !dataReader.IsClosed)
+                {
+                    dataReader.Close();
+                }
+            }
         }
     }
 }
